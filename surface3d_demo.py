e0a2476f7a7a5df9f278b8c3868d0080e8262e9a
Joshua Shelton <joshua@joshuashelton.com>
2019-06-26 21:48:05 +0300

    Initial Test

--- surface3d_demo.py
+++ surface3d_demo.py
@@ -21,18 +21,22 @@ fig = plt.figure()
 ax = fig.gca(projection='3d')
 
 # Make data.
-X = np.arange(-5, 5, 0.25)
-Y = np.arange(-5, 5, 0.25)
+X = np.arange(-10, 10, 0.25)
+Y = np.arange(-10, 10, 0.25)
 X, Y = np.meshgrid(X, Y)
-R = np.sqrt(X**2 + Y**2)
-Z = np.sin(R)
+Z = X**2-Y**2
+W = X+Y/2
 
 # Plot the surface.
 surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
                        linewidth=0, antialiased=False)
 
+surf = ax.plot_surface(X, Y, W, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+
 # Customize the z axis.
-ax.set_zlim(-1.01, 1.01)
+ax.set_zlim(-100.01, 100.01)
 ax.zaxis.set_major_locator(LinearLocator(10))
 ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
 
