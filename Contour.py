57f534332996c097b74df7c5b3d316fdd3d632e1
Joshua Shelton <joshua@joshuashelton.com>
2019-06-27 13:09:57 +0300

    Refactor

--- Contour.py
+++ Contour.py
@@ -5,28 +5,28 @@ from matplotlib.ticker import MaxNLocator
 import numpy as np
 
 
-# Theta
+# Theta (X axis)
 theta_min, theta_max = 1, 30
-xmin, xmax = theta_min, theta_max
+Xmin, Xmax = theta_min, theta_max
 
-# Z
+# Z (Y axis)
 Zmin, Zmax = 1, 30
-ymin, ymax = Zmin, Zmax
+Ymin, Ymax = Zmin, Zmax
 
 
 # make these smaller to increase the resolution
 dx, dy = 0.1, 0.1
 
 
-# generate 2 2d grids for the x & y bounds
-Y, X = np.mgrid[slice(ymin, ymax + dy, dy),
-                slice(xmin, xmax + dx, dx)]
+# generate 2 2d grids for the X & Y bounds
+Y_mat, X_mat = np.mgrid[slice(Ymin, Ymax + dy, dy),
+                        slice(Xmin, Xmax + dx, dx)]
 
 
-# c = np.sin(x)**10 + np.cos(10 + y*x) * np.cos(x)
-c = np.zeros((len(Y), len(Y[0])))
+# c = np.sin(X)**10 + np.cos(10 + Y*X) * np.cos(X)
+c = np.zeros((len(Y_mat), len(Y_mat[0])))
 
-aik = np.zeros(len(Y))
+aik_arr = np.zeros(len(Y_mat))
 
 
 q1 = 1.5
@@ -35,26 +35,43 @@ Q1 = 5
 Q2 = 5
 p1 = 2
 p2 = 3
-lambdaL = 40
+Lambdas = [40, 40, 40]
+# Assumes that additional products are added at the end and not in the middle
 
-for i in range(len(Y)):
-    aik[i] = (Y[i][0]**2/2)**(1/3)
-    A = aik[i]
-    I = aik[i]
-    K = aik[i]
+for i in range(len(Y_mat)):
+    Z = Y_mat[i][0]
 
-    for j in range(len(Y[0])):
-        x = X[i][j]
-        y = Y[i][j]
-        # if ((x/5)**2 > (y/5)**3):
+    aik = (Z**2/2)**(1/3)
+
+    aik_arr[i] = aik
+    A = aik
+    I = aik
+    K = aik
+
+    for j in range(len(Y_mat[0])):
+        X = X_mat[i][j]
+        Y = Y_mat[i][j]
+
+        Theta = X
+
+        # if ((X/5)**2 > (Y/5)**3):
         #     c[i][j] = 2
-        # if (x + y < 4):
+        # if (X + Y < 4):
         #     c[i][j] = 1
-        if (Y[i][0] < theta_min*lambdaL**.5):
+
+        if (Y_mat[i][0] < theta_min*Lambdas[0]**.5):
             c[i][j] = 1
 
+        # Case i - two products
 
-# x and y are bounds, so c should be the value *inside* those bounds.
+        # sum = 0
+        # for k in range(2):
+        #     sum += (Lambdas[k]**.5 - Z)**2
+
+        # prodi = sum/(4*A)
+
+
+# X and Y are bounds, so c should be the value *inside* those bounds.
 # Therefore, remove the last value from the c array.
 c = c[:-1, :-1]
 levels = MaxNLocator(nbins=15).tick_values(c.min(), c.max())
@@ -66,18 +83,19 @@ cmap = plt.get_cmap('PiYG')
 norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
 
 
-# fig, (ax1, ax0) = plt.subplots(nrows=1)
 fig, (ax1) = plt.subplots()
 
-# im = ax0.pcolormesh(X, Y, c, cmap=cmap, norm=norm)
+# If you wanted to plot two images
+# fig, (ax1, ax0) = plt.subplots(nrows=1)
+# im = ax0.pcolormesh(X_mat, Y_mat, c, cmap=cmap, norm=norm)
 # fig.colorbar(im, ax=ax0)
 # ax0.set_title('pcolormesh with levels')
 
 
 # contours are *point* based plots, so convert our bound into point
 # centers
-cf = ax1.contourf(X[:-1, :-1] + dx/2.,
-                  Y[:-1, :-1] + dy/2., c, levels=levels,
+cf = ax1.contourf(X_mat[:-1, :-1] + dx/2.,
+                  Y_mat[:-1, :-1] + dy/2., c, levels=levels,
                   cmap=cmap)
 fig.colorbar(cf, ax=ax1)
 ax1.set_title('contourf with levels')
